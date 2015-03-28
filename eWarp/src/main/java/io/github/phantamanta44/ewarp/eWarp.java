57e386ecad9788601ae9141fff38529f5e507b7d
Phanta <evandalong@gmail.com>
2015-03-28 18:35:42 -0500

    Main class

---
+++ eWarp/src/main/java/io/github/phantamanta44/ewarp/eWarp.java
@@ -0,0 +1,21 @@
+package io.github.phantamanta44.ewarp;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class eWarp extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		return true;
+	}
+
+}
